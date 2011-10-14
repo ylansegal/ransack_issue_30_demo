7b99e047feb6c9eec6b437c373ec54ecc8ea4fd3
Ylan Segal <ylan@alxglobal.com>
2011-10-14 14:07:47 -0700

    Empty Rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RansackBugApp::Application.config.secret_token = '28bc7869b58848ece102ba1bcce552587318df7e53e69e1fb004a423a4d1aceb272058f6fb60b3a5e0d9851e1e6751175faa8caf8c7d4781b1e2831af3347f00'
